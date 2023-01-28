5c809fa1964e05f1b24518a4e08364efbcc3150e
jhih_yu <jy@jhihyulin.live>
2023-01-28 15:54:25 +0800

    Update VPN.dart
    fix import

--- lib/pages/VPN.dart
+++ lib/pages/VPN.dart
@@ -1,9 +1,7 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:http/http.dart' as http;
-import 'dart:convert';
 import 'package:firebase_auth/firebase_auth.dart';
 
 const String VPNSERVER_DOMAIN = 'vpn.jhihyulin.live';

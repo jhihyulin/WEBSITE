2071b36b84fc7c5fcb4a4baade019ba8aaddaf93
jhih_yu <jy@jhihyulin.live>
2023-01-30 20:26:07 +0800

    Rewrite social widget generate method

--- lib/pages/About.dart
+++ lib/pages/About.dart
@@ -6,23 +6,68 @@ import 'package:animated_text_kit/animated_text_kit.dart';
 const String Text_1 = 'Not yet written.';
 const String Text_2 = '';
 
-final Uri GitHub_url = Uri.parse('https://github.com/jhihyulin');
-final Uri LinkedIn_url = Uri.parse('https://linkedin.com/in/jhihyulin');
-final Uri GitLab_url = Uri.parse('https://gitlab.com/jhihyulin');
-final Uri Twitter_url = Uri.parse('https://twitter.com/jhih_yu_lin');
-final Uri Facebook_url = Uri.parse('https://facebook.com/jhihyu0414');
-final Uri Instagram_url = Uri.parse('https://instagram.com/jhih_yu_lin');
-final Uri YouTube_url = Uri.parse('https://youtube.com/@jhihyulin');
-final Uri Twitch_url = Uri.parse('https://twitch.tv/jhih_yu_lin');
-final Uri GoogleDeveloper_url = Uri.parse('https://g.dev/jhihyulin');
-final Uri StackOverflow_url =
-    Uri.parse('https://stackoverflow.com/users/15607217/jhih-yu-lin');
-final Uri Spotify_url =
-    Uri.parse('https://open.spotify.com/user/ylde507yo3uxfvshedazdr88n');
-final Uri Medium_url = Uri.parse('https://medium.com/@jhihyulin');
-final Uri Email_url = Uri.parse('mailto:jy@jhihyulin.live');
-final Uri Telegram_url = Uri.parse('https://t.me/jhihyulin');
-final Uri SoundCloud_url = Uri.parse('https://soundcloud.com/jhihyulin');
+Map<String, Map<String, Object>> SocialMedia = {
+  'GitHub': {
+    'url': 'https://github.com/jhihyulin',
+    'icon': Logo.github,
+  },
+  'LinkedIn': {
+    'url': 'https://linkedin.com/in/jhihyulin',
+    'icon': Logo.linkedin,
+  },
+  'GitLab': {
+    'url': 'https://gitlab.com/jhihyulin',
+    'icon': Logo.gitlab,
+  },
+  'Twitter': {
+    'url': 'https://twitter.com/jhih_yu_lin',
+    'icon': Logo.twitter,
+  },
+  'Facebook': {
+    'url': 'https://facebook.com/jhihyu0414',
+    'icon': Logo.facebook,
+  },
+  'Instagram': {
+    'url': 'https://instagram.com/jhih_yu_lin',
+    'icon': Logo.instagram,
+  },
+  'YouTube': {
+    'url': 'https://youtube.com/@jhihyulin',
+    'icon': Logo.youtube,
+  },
+  'Twitch': {
+    'url': 'https://twitch.tv/jhih_yu_lin',
+    'icon': Logo.twitch,
+  },
+  'GoogleDeveloper': {
+    'url': 'https://g.dev/jhihyulin',
+    'icon': Logo.google,
+  },
+  'StackOverflow': {
+    'url': 'https://stackoverflow.com/users/15607217/jhih-yu-lin',
+    'icon': Logo.stack_overflow,
+  },
+  'Spotify': {
+    'url': 'https://open.spotify.com/user/ylde507yo3uxfvshedazdr88n',
+    'icon': Logo.spotify,
+  },
+  'Medium': {
+    'url': 'https://medium.com/@jhihyulin',
+    'icon': Logo.medium_brand,
+  },
+  'Email': {
+    'url': 'mailto:jy@jhihyulin.live',
+    'icon': Icons.email,
+  },
+  'Telegram': {
+    'url': 'https://t.me/jhihyulin',
+    'icon': Logo.telegram,
+  },
+  'SoundCloud': {
+    'url': 'https://soundcloud.com/jhihyulin',
+    'icon': Logo.soundcloud,
+  },
+};
 
 class AboutPage extends StatelessWidget {
   @override
@@ -32,97 +77,43 @@ class AboutPage extends StatelessWidget {
         title: Text('About'),
       ),
       body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              AnimatedTextKit(
-                isRepeatingAnimation: false,
-                displayFullTextOnTap: true,
-                animatedTexts: [
-                  TypewriterAnimatedText(
-                    Text_1,
-                    textAlign: TextAlign.center,
-                    speed: const Duration(milliseconds: 50),
-                  ),
-                ],
-              ),
-              //Text(Text_2),
-              SizedBox(height: 20),
-              Container(
-                  constraints: BoxConstraints(maxWidth: 700),
-                  padding: const EdgeInsets.all(10),
-                  child: Wrap(
-                    spacing: 10,
-                    runSpacing: 10,
-                    alignment: WrapAlignment.center,
-                    children: [
+          child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            AnimatedTextKit(
+              isRepeatingAnimation: false,
+              displayFullTextOnTap: true,
+              animatedTexts: [
+                TypewriterAnimatedText(
+                  Text_1,
+                  textAlign: TextAlign.center,
+                  speed: const Duration(milliseconds: 50),
+                ),
+              ],
+            ),
+            //Text(Text_2),
+            SizedBox(height: 20),
+            Container(
+                constraints: BoxConstraints(maxWidth: 700),
+                padding: const EdgeInsets.all(10),
+                child: Wrap(
+                  spacing: 10,
+                  runSpacing: 10,
+                  alignment: WrapAlignment.center,
+                  children: [
+                    for (var key in SocialMedia.keys)
                       ElevatedButton.icon(
-                          icon: const Icon(Logo.github),
-                          label: Text('GitHub'),
-                          onPressed: () => _launchUrl(GitHub_url)),
-                      ElevatedButton.icon(
-                          icon: const Icon(Logo.gitlab),
-                          label: Text('GitLab'),
-                          onPressed: () => _launchUrl(GitLab_url)),
-                      ElevatedButton.icon(
-                          icon: const Icon(Logo.linkedin),
-                          label: Text('LinkedIn'),
-                          onPressed: () => _launchUrl(LinkedIn_url)),
-                      ElevatedButton.icon(
-                          icon: const Icon(Logo.twitter),
-                          label: Text('Twitter'),
-                          onPressed: () => _launchUrl(Twitter_url)),
-                      ElevatedButton.icon(
-                          icon: const Icon(Logo.facebook),
-                          label: Text('Facebook'),
-                          onPressed: () => _launchUrl(Facebook_url)),
-                      ElevatedButton.icon(
-                          icon: const Icon(Logo.instagram),
-                          label: Text('Instagram'),
-                          onPressed: () => _launchUrl(Instagram_url)),
-                      ElevatedButton.icon(
-                          icon: const Icon(Logo.youtube),
-                          label: Text('YouTube'),
-                          onPressed: () => _launchUrl(YouTube_url)),
-                      ElevatedButton.icon(
-                          icon: const Icon(Logo.twitch),
-                          label: Text('Twitch'),
-                          onPressed: () => _launchUrl(Twitch_url)),
-                      ElevatedButton.icon(
-                          icon: const Icon(Logo.google_developers),
-                          label: Text('Google Developer'),
-                          onPressed: () => _launchUrl(GoogleDeveloper_url)),
-                      ElevatedButton.icon(
-                          icon: const Icon(Logo.stack_overflow),
-                          label: Text('Stack Overflow'),
-                          onPressed: () => _launchUrl(StackOverflow_url)),
-                      ElevatedButton.icon(
-                          icon: const Icon(Logo.spotify),
-                          label: Text('Spotify'),
-                          onPressed: () => _launchUrl(Spotify_url)),
-                      ElevatedButton.icon(
-                          icon: const Icon(Logo.medium_brand),
-                          label: Text('Medium'),
-                          onPressed: () => _launchUrl(Medium_url)),
-                      ElevatedButton.icon(
-                          icon: const Icon(Icons.email),
-                          label: Text('Email'),
-                          onPressed: () => _launchUrl(Email_url)),
-                      ElevatedButton.icon(
-                          icon: const Icon(Logo.telegram),
-                          label: Text('Telegram'),
-                          onPressed: () => _launchUrl(Telegram_url)),
-                      ElevatedButton.icon(
-                          icon: const Icon(Logo.soundcloud),
-                          label: Text('SoundCloud'),
-                          onPressed: () => _launchUrl(SoundCloud_url)),
-                    ],
-                  ))
-            ],
-          ),
-        )
-      ),
+                        onPressed: () => _launchUrl(Uri.parse(
+                            SocialMedia[key]!['url'] as String)),
+                        icon: Icon(SocialMedia[key]!['icon'] as IconData),
+                        label: Text(key),
+                      ),
+                  ],
+                ))
+          ],
+        ),
+      )),
     );
   }
 }

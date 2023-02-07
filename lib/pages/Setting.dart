35514e4794d1337c0de9f23e24fabcad3581ba88
jhih_yu <jy@jhihyulin.live>
2023-02-07 11:55:21 +0800

    remove unuse

--- lib/pages/Setting.dart
+++ lib/pages/Setting.dart
@@ -1,7 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 

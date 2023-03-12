3694b965514f94e641dd7a96ed40dd729c6cea70
jhih_yu <jy@jhihyulin.live>
2023-03-12 22:00:38 +0800

    fix build1 shape

--- lib/pages/zhsh_3d_map.dart
+++ lib/pages/zhsh_3d_map.dart
@@ -136,6 +136,9 @@ const Map settingData = {
       'build1_beam_###4': {'name': '行政大樓-梁5', 'searchable': false},
       'build1_beam_###5': {'name': '行政大樓-梁6', 'searchable': false},
       'build1_beam_###6': {'name': '行政大樓-梁7', 'searchable': false},
+      'build1_beam_###7': {'name': '行政大樓-梁8', 'searchable': false},
+      'build1_beam_###8': {'name': '行政大樓-梁9', 'searchable': false},
+      'build1_beam_###9': {'name': '行政大樓-梁10', 'searchable': false},
       'build1_facade': {'name': '行政大樓-牆1', 'searchable': false},
       'build1_gate': {'name': '行政大樓-大門', 'searchable': false},
       'build1_b1_room1': {
@@ -194,6 +197,10 @@ const Map settingData = {
         'link': {
           '校長室網站': 'https://sites.google.com/mail2.chshs.ntpc.edu.tw/principal'
         }
+      },
+      'build1_3f_room2_###extend': {
+        'name': '校長室露臺',
+        'searchable': false
       },
       'build1_3f_room3': {
         'name': '會計室',
@@ -627,7 +634,7 @@ const Map<String, Map<String, dynamic>> mapData = {
     'x': -14.1,
     'y': 1,
     'z': 74.6,
-    'height': 7,
+    'height': 10,
     'width': 0.8,
     'length': 0.8
   },
@@ -636,7 +643,7 @@ const Map<String, Map<String, dynamic>> mapData = {
     'x': -14.1,
     'y': 1,
     'z': 69.7,
-    'height': 7,
+    'height': 10,
     'width': 0.8,
     'length': 0.8
   },
@@ -737,6 +744,33 @@ const Map<String, Map<String, dynamic>> mapData = {
     'z': 47.35,
     'height': 0.5,
     'width': 4.7,
+    'length': 0.5
+  },
+  'build1_beam_###7': {
+    'build': 'build1',
+    'x': -11.6,
+    'y': 7,
+    'z': 74.6,
+    'height': 2,
+    'width': 0.5,
+    'length': 4.2
+  },
+  'build1_beam_###8': {
+    'build': 'build1',
+    'x': -14.1,
+    'y': 7,
+    'z': 72.15,
+    'height': 2,
+    'width': 4.1,
+    'length': 0.5
+  },
+  'build1_beam_###9': {
+    'build': 'build1',
+    'x': -14.1,
+    'y': 7,
+    'z': 67.25,
+    'height': 2,
+    'width': 4.1,
     'length': 0.5
   },
   'build1_facade': {
@@ -916,11 +950,19 @@ const Map<String, Map<String, dynamic>> mapData = {
     'build': 'build1',
     'x': -12,
     'y': 8,
-    'z': 62,
+    'z': 57.1,
     'height': 3,
-    'width': 26,
+    'width': 16.2,
     'length': 5,
     'floor': 3,
+  },
+  'build1_3f_room2_###extend': {
+    'x': -15.25,
+    'y': 8,
+    'z': 55,
+    'height': 1.5,
+    'width': 14,
+    'length': 1.5
   },
   'build1_3f_room3': {
     'build': 'build1',

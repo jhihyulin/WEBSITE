a114a98508866797c247e4bc5faee6456be60428
jhih_yu <jy@jhihyulin.live>
2023-04-08 19:54:20 +0800

    add TODO

--- lib/main.dart
+++ lib/main.dart
@@ -80,6 +80,7 @@ class _MyAppState extends State<MyApp> {
                     borderRadius: BorderRadius.all(Radius.circular(16.0)),
                     child: LinearProgressIndicator(
                       minHeight: 20,
+                      // TODO: add load progress
                       value: null,
                     ),
                   ),

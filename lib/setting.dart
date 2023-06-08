e64064cef97815e990a7340ce27bb7dbefdfb65a
jhih_yu <jy@jhihyulin.live>
2023-06-08 10:10:00 +0800

    [Fix] fix import

--- lib/setting.dart
+++ lib/setting.dart
@@ -3,9 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 import 'package:url_launcher/url_launcher.dart';
-
-import 'provider/theme.dart';
 import 'package:icons_plus/icons_plus.dart';
+
+import '../provider/theme.dart';
 
 class SettingPage extends StatefulWidget {
   const SettingPage({super.key});
